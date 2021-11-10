518585f177699e2f8e67c129d1796c42640d7cf5
Ricardo-2021 <rickguimaraes1976@hotmail.com>
2021-11-10 14:56:39 -0300

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,58 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: string;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Apartamento 2 dormitórios com sacada',
+    price: 'Diaria R$ 100   Contato: (51)999999999',  
+    description: 'Av. Assis Brasil, 2525, cep: 95852-250 '
+  },
+  {
+    id: 2,
+    name: 'Casa 3 Quartos',
+    price: 'Diaria R$ 200',
+    description: 'Balneário Camboriú contato (51)99999999'
+  },
+  {
+    id: 3,
+    name: 'Pousada ',
+    price: 'Diaria R$ 80',
+    description: 'Curitiba contato (51)99999999'
+  },
+  {
+    id: 4,
+    name: 'Apartamento 2 dormitórios',
+    price: 'Diaria R$ 120',  
+    description: 'São Paulo contato (51)99999999'
+  },
+  {
+    id: 5,
+    name: 'Casa 1 Suite e 2 Quartos',
+    price: 'Diaria R$ 280',  
+    description: 'São Paulo contato (51)99999999'
+  },
+  {
+    id: 6,
+    name: 'Casa 2 Suite e 4 Quartos',
+    price: 'Diaria R$ 400',  
+    description: 'Fortaleza contato (51)99999999'
+  },
+  {
+    id: 7,
+    name: 'Cobertura 3 dormitórios ',
+    price: 'Diaria R$ 500',  
+    description: 'Florianopolis contato (51)99999999'
+  },
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
