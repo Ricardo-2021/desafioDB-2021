b3bd9475fbe11ada385f9ea2d2dca30136d0cd25
Ricardo-2021 <rickguimaraes1976@hotmail.com>
2021-11-15 01:14:29 -0300

    #8

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,49 +1,49 @@
 export interface Product {
   id: number;
-  name: string;
-  price: string;
-  description: string;
+  imovel: string;
+  valorContato: string;
+  enderecoDisponibilidade: string;
  
 }
 
 export const products = [
   {
     id: 1,
-    name: 'Casa com dois dormitórios e com sacada',
-    price: 'Diaria de R$ 100,00 contato (51)998523265',  
-    description: 'Av. Caxias, 2525,Imbé, disponivel janeiro,fevereiro e março de 2022',
+    imovel: 'Casa com dois dormitórios e com sacada',
+    valorContato: 'Diaria de R$ 100,00 contato (51)998523265',  
+    enderecoDisponibilidade: 'Av. Caxias, 2525,Imbé, disponivel janeiro,fevereiro e março de 2022',
     
   },
   {
     id: 2,
-    name: 'Apt dois dormitórios e área de serviço',
-    price: 'Diaria de R$ 80,00 contato (51)983542600',
-    description: 'Av. Emancipação, 2526, tramandaí, disponivel janeiro e fevereiro de 2022',
+    imovel: 'Apt dois dormitórios e área de serviço',
+    valorContato: 'Diaria de R$ 80,00 contato (51)983542600',
+    enderecoDisponibilidade: 'Av. Emancipação, 2526, tramandaí, disponivel janeiro e fevereiro de 2022',
     
   },
   {
     id: 3,
-    name: 'Apt três dormitórios com uma suite ',
-    price: 'Diaria de R$ 110,00 contato (51)965274123',
-    description: 'Av. Osório, 951, Tramandaí disponivel janeiro, fevereiro e março de 2022'
+    imovel: 'Apt três dormitórios com uma suite ',
+    valorContato: 'Diaria de R$ 110,00 contato (51)965274123',
+    endercoDisponibilidade: 'Av. Osório, 951, Tramandaí disponivel janeiro, fevereiro e março de 2022'
   },
   {
     id: 4,
-    name: 'Casa com três dormitórios com uma suite',
-    price: 'Diaria de R$ 150,00 contato (51)998523145',  
-    description: 'Av. Gravataí, 131, Imbé disponivel janeiro  de 2022'
+    imovel: 'Casa com três dormitórios com uma suite',
+    valorContato: 'Diaria de R$ 150,00 contato (51)998523145',  
+    enderecodisponibilidade: 'Av. Gravataí, 131, Imbé disponivel janeiro  de 2022'
   },
   {
     id: 5,
-    name: 'Apt com dois dormitórios e com sacada',
-    price: 'Diaria de R$ 100,00 contato (51)973258745',  
-    description: 'Av. Novo hamburgo, 552, disponivel janeiro e fevereiro de 2022'
+    imovel: 'Apt com dois dormitórios e com sacada',
+    valorContato: 'Diaria de R$ 100,00 contato (51)973258745',  
+    enderecoDisponibilidade: 'Av. Novo hamburgo, 552, disponivel janeiro e fevereiro de 2022'
   },
   {
     id: 6,
-    name: 'Casa com dois dormitórios, com sacada, garagem para dois carros',
-    price: 'Diaria de R$ 120,00 contato (51)965852321',  
-    description: 'Av. Assis Brasil, 2525, disponivel janeiro e fevereiro de 2022'
+    imovel: 'Casa com dois dormitórios, com sacada, garagem para dois carros',
+    valorContato: 'Diaria de R$ 120,00 contato (51)965852321',  
+    enderecoDisponibilidade: 'Av. Assis Brasil, 2525, disponivel janeiro e fevereiro de 2022'
   },
   
 ];
